b61187e113b540daa4ad98fc2b53b6794d474554
geff <zomg21@hotmail.com>
2017-07-13 10:37:02 +0100

    Arcs on canvases from template-01 generate on load when exported

--- js/templates/template-01.js
+++ js/templates/template-01.js
@@ -14,6 +14,25 @@ $(document).on('input', '.template-01-input-field', function() {
 });
 
 
+$(document).ready(function(){
+	var pickRates = [];
+	var banRates= [];
+
+	for(var i=1; i<=3; i++)
+	{
+		pickRates.push($(".template-01-pick-rate-"+i).text());
+		banRates.push($(".template-01-ban-rate-"+i).text());
+	}
+
+	var canvas="template-01-canvas-";
+	for(var i=0; i<3; i++)
+	{
+		arc(pickRates[i], canvas+(1+i), true); //pick rates
+		arc(banRates[i], 	canvas+(4+i), false); //ban rates
+	}
+});
+
+
 
 //percent should be 0-1 value
 function arc(percent, canvasID, isGreen)
