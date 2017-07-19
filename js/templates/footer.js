ab3109691f82e08c2e0708f411f39ef4abf77c8e
geff <zomg21@hotmail.com>
2017-07-19 11:05:05 +0100

    changed logo gradiant and added it to footer

--- js/templates/footer.js
+++ js/templates/footer.js
@@ -3,7 +3,8 @@ $(document).on('click', '#footer', function(){
 	var html =
     '<div class="row">' +
       '<div class="col-md-3">' +
-        '<img src="img/footer/twitter_icon.png" class="twitter">' +
+				'<a href="https://twitter.com/PohkaDota" target="_blank">' +
+        '<img src="img/footer/twitter_icon.png" class="twitter"></a>' +
       '</div>' +
       '<div class="col-md-6 footer-text">' +
         'twitter.com/PohkaDota' +
@@ -11,7 +12,8 @@ $(document).on('click', '#footer', function(){
       '<div class="col-md-3">' +
         '<img src="img/footer/ogre.png" class="avatar">' +
       '</div>' +
-    '</div>';
+    '</div>' +
+		'<div><img src="img/logo/logo_sm.png" height="20"></div>';
 
     setBodyHtml(html, this);
 });
