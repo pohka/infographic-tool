d2bdbdd5a41463055306ea443b535d2781e150a2
geff <zomg21@hotmail.com>
2017-07-20 17:50:30 +0100

    added simple functionality to navbar and new font to website

--- js/cooldownfeed.js
+++ js/cooldownfeed.js
@@ -1,13 +1,22 @@
-//logo animation when you hover
+
 $(document).ready(function() {
+
+  //logo animation when you hover
   $(".logo-letter-span").mouseenter(function(){
          $(".logo-letter").animate({ "padding-right": "+=30px" }, "fast" );
         $(".logo-text").fadeIn('fast');
   });
 
   $(".logo-letter-span").mouseleave(function(event) {
-
      $(".logo-letter").animate({ "padding-right": "-=50px" }, "fast" );
      $(".logo-text").hide();
+  });
+
+  $(".nav-item").click(function(){
+    var name = $(this).data("name");
+    $(".section").each(function(index, el) {
+      $(this).hide();
+    });
+    $("#"+name).show();
   });
 });
