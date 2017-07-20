81c9efd285c2cee73526583b8e6ac244c342157d
geff <zomg21@hotmail.com>
2017-07-20 12:05:19 +0100

    navbar is now centered on page

--- js/cooldownfeed.js
+++ js/cooldownfeed.js
@@ -4,16 +4,14 @@ $(document).ready(function() {
   $(".logo-letter-span").mouseenter(function(){
       //  $(".logo-letter-span").css("width", "215px");
          $(".logo-letter").animate({ "padding-right": "+=30px" }, "fast" );
-         $(".cdf-navitems").animate({ "padding-left": "+=150px" }, "fast" )
         $(".logo-text").fadeIn('fast');
+
   });
 
   $(".logo-letter-span").mouseleave(function(event) {
 
      $(".logo-letter").animate({ "padding-right": "-=50px" }, "fast" );
-     $(".cdf-navitems").animate({ "padding-left": "-=150px" }, "fast" )
      $(".logo-text").hide();
-    // $(".logo-letter-span").css("width", "100px");
   });
 
 });
