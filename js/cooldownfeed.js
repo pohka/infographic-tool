6287c0d1bb11b13fa43bef36615016a41c185bea
geff <zomg21@hotmail.com>
2017-07-21 11:18:33 +0100

    Added navbar drop down

--- js/cooldownfeed.js
+++ js/cooldownfeed.js
@@ -1,3 +1,4 @@
+var hovering={a:false,b:false};
 
 $(document).ready(function() {
 
@@ -12,11 +13,28 @@ $(document).ready(function() {
      $(".logo-text").hide();
   });
 
-  $(".nav-item").click(function(){
-    var name = $(this).data("name");
-    $(".section").each(function(index, el) {
-      $(this).hide();
-    });
-    $("#"+name).show();
+
+  $(".nav-dropdown-btn").hover(function(){
+    $(".nav-dropdown-menu").show();
+  });
+
+  $('#a, #b').mouseover(function(){
+    hovering [$(this).attr('id')] = true;
+  });
+
+  $('#a, #b').mouseout(function(){
+      hovering [$(this).attr('id')] = false;
+
+      setTimeout(function(){
+            if(!hovering.a && !hovering.b)
+                $('.nav-dropdown-menu').fadeOut(200)
+      },100);
   });
 });
+
+function hideDropDown()
+{
+  console.log(hovering["dropDownBtn"] +","+ hovering["dropDownMenu"]);
+  if(hovering["dropDownBtn"]==false && hovering["dropDownMenu"]==false)
+    $(".nav-dropdown-menu").hide();
+}
