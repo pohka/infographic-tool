0507f0c7914fe00d7e70fd7ea9d8c6b48b28d686
geff <zomg21@hotmail.com>
2017-07-29 04:04:16 +0100

    moved footer and navbar css to seperate files

--- js/templates/footer3.js
+++ js/templates/footer3.js
@@ -1,6 +1,9 @@
 $(document).on("click", "#footer3", function(){
   var html=
-  '<div class="footer3-text"></div>';
+  '<div class="footer3-text">'+
+  'All match data is from 7.05 to the patch before TI7, unless specified otherwise<br>April 9th-July 30th 2017'+
+  '<br>References: OpenDota and Dotabuff'+
+  '</div>';
 
   setBodyHtml(html,this);
 })
